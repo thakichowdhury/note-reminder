2d1a7c86d27bfe37aa122a304edc975b48cd5b88
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-22 22:53:18 -0700

    Add endpoint to receive messages

--- src/server/start.js
+++ src/server/start.js
@@ -1,6 +1,18 @@
+const app = require('./index.js');
+
 const PORT = process.env.PORT || 3000;
 
-const app = require('./index.js');
+// const receiveMessage = require('../lib/twilio/receiveMessage.js');
+
+// TODO: break endpoints into a separate routes file
+app.post('/sms', (req, res) => {
+  console.log(req.body);
+  const message = req.body.Body;
+  // const twiml = receiveMessage('TESTING RESPONSE TO MESSAGE');
+
+  // res.writeHead(200, { 'Content-Type': 'text/xml' });
+  // res.end(twiml.toString());
+});
 
 app.listen(
   PORT,
