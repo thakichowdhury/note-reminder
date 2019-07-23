cdc0f3d9565a6106898af8566e5c1fcaeaa35fef
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-23 08:31:28 -0700

    Remove extraneous code

--- src/server/start.js
+++ src/server/start.js
@@ -2,16 +2,10 @@ const app = require('./index.js');
 
 const PORT = process.env.PORT || 3000;
 
-// const receiveMessage = require('../lib/twilio/receiveMessage.js');
-
 // TODO: break endpoints into a separate routes file
 app.post('/sms', (req, res) => {
   console.log(req.body);
   const message = req.body.Body;
-  // const twiml = receiveMessage('TESTING RESPONSE TO MESSAGE');
-
-  // res.writeHead(200, { 'Content-Type': 'text/xml' });
-  // res.end(twiml.toString());
 });
 
 app.listen(
