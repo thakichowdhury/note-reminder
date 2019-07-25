2b48e93a6c2564b148f5da8fc4a69a9b513779de
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-25 09:46:03 -0700

    Update sms endpoint

--- src/server/start.js
+++ src/server/start.js
@@ -1,11 +1,21 @@
+const moment = require('moment');
+
 const app = require('./index.js');
 
 const PORT = process.env.PORT || 3000;
 
+const db = require('../db');
+
 // TODO: break endpoints into a separate routes file
-app.post('/sms', (req, res) => {
-  console.log(req.body);
-  const message = req.body.Body;
+app.post('/sms', async (request, response) => {
+  const message = request.body.Body;
+
+  const createdAt = moment().format('YYYY-MM-DD HH:MM:SS');
+  const query = `INSERT into notes (message, created_at, last_called) VALUES ('${message}', TO_TIMESTAMP('${createdAt}', 'YYYY-MM-DD HH:MI:SS'), TO_TIMESTAMP('${createdAt}', 'YYYY-MM-DD HH:MI:SS'));`;
+
+  db.query(query)
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
 });
 
 app.listen(
