c402f6e3b59010f861d1425d73af09c7c4bce16f
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-26 01:29:06 -0700

    Remove moment import with util function

--- src/db/actions/insertNewMessage.js
+++ src/db/actions/insertNewMessage.js
@@ -1,12 +1,12 @@
-const moment = require('moment');
-
 const db = require('../index');
+
+const getCurrentTimestamp = require('../../util/getCurrentTimestamp');
 
 const insertNewMessage = ({
   phoneString,
   message,
 }) => {
-  const createdAt = moment().format('YYYY-MM-DD hh:mm:ss A');
+  const createdAt = getCurrentTimestamp();
   const phoneNumber = parseInt(phoneString.slice(2), 10);
 
   const query = 'INSERT into notes (phone_number, message, created_at) VALUES ($1, $2, $3);';
