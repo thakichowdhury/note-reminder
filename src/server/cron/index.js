ccc712ad8d194771ea4224f7f145103bfe02020c
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-26 00:03:21 -0700

    Create message sending cron-job

---
+++ src/server/cron/index.js
@@ -0,0 +1,3 @@
+const sendMessageCron = require('./sendMessageCron');
+
+exports = { sendMessageCron };
---
+++ src/server/cron/sendMessageCron.js
@@ -0,0 +1,21 @@
+const cron = require('node-cron');
+const db = require('../../db');
+
+const createMessage = require('../../lib/twilio/createMessage');
+const getRandomInt = require('../../util/getRandomInt');
+
+const sendMessageCron = cron.schedule('*/10 * * * * *', async () => {
+  const { rows: [{ count }] } = await db.query('SELECT COUNT(*) FROM notes;');
+
+  const randomId = getRandomInt(1, count);
+
+  const { rows } = await db.query('SELECT message FROM notes WHERE id = $1;', [randomId]);
+  const message = rows.length && rows[0].message;
+
+  if (message) {
+    console.log(message);
+    createMessage({ message });
+  }
+});
+
+exports = sendMessageCron;
