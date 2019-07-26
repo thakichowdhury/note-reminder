d05ae1dca172db691f719b3a08e6bc15980083da
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-26 00:21:35 -0700

    Refactor api to reflect seperation of db actions

--- src/server/start.js
+++ src/server/start.js
@@ -1,25 +1,14 @@
-const moment = require('moment');
-
 const app = require('./index.js');
-const db = require('../db');
-const { logEveryMinute } = require('./cron');
+
+const insertNewMessage = require('../db/actions/insertNewMessage');
+
+// start cron-jobs
+require('./cron');
 
 const PORT = process.env.PORT || 3000;
 
 // TODO: break endpoints into a separate routes file
-app.post('/sms', (request, response) => {
-  const message = request.body.Body;
-  const createdAt = moment().format('YYYY-MM-DD hh:mm:ss A');
-
-  const query = `INSERT into notes (message, created_at) VALUES ($1, $2);`;
-  const values = [message, createdAt];
-
-  db.query(query, values)
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
-});
-
-logEveryMinute.start();
+app.post('/sms', ({ body }) => insertNewMessage(body.Body));
 
 app.listen(
   PORT,
