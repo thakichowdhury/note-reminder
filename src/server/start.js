0ad778eae13c2714dd9210202b01d30844f43fb8
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-25 23:47:30 -0700

    Update moment js formatting to keep minutes and seconds under 60

--- src/server/start.js
+++ src/server/start.js
@@ -1,15 +1,15 @@
 const moment = require('moment');
 
 const app = require('./index.js');
+const db = require('../db');
+const { logEveryMinute } = require('./cron');
 
 const PORT = process.env.PORT || 3000;
-
-const db = require('../db');
 
 // TODO: break endpoints into a separate routes file
 app.post('/sms', (request, response) => {
   const message = request.body.Body;
-  const createdAt = moment().format('YYYY-MM-DD hh:MM:SS A');
+  const createdAt = moment().format('YYYY-MM-DD hh:mm:ss A');
 
   const query = `INSERT into notes (message, created_at) VALUES ($1, $2);`;
   const values = [message, createdAt];
@@ -18,6 +18,8 @@ app.post('/sms', (request, response) => {
     .then(res => console.log(res))
     .catch(err => console.error(err));
 });
+
+logEveryMinute.start();
 
 app.listen(
   PORT,
