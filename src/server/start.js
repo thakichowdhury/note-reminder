155793951d7550b8f9344b5f72f3d4c540c90bda
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-25 22:42:18 -0700

    Remove async operator

--- src/server/start.js
+++ src/server/start.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const db = require('../db');
 
 // TODO: break endpoints into a separate routes file
-app.post('/sms', async (request, response) => {
+app.post('/sms', (request, response) => {
   const message = request.body.Body;
   const createdAt = moment().format('YYYY-MM-DD hh:MM:SS A');
 
