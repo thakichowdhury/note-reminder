85df805fd74bc4a86691fd59269e9690c7c4f34d
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-27 16:08:49 -0700

    Add logging to ssh script

--- src/scripts/connect_to_remote_server.sh
+++ src/scripts/connect_to_remote_server.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-ssh -i "secrets/quote_reminder.pem" ec2-user@ec2-54-237-170-188.compute-1.amazonaws.com
+instance_address="ec2-54-237-170-188.compute-1.amazonaws.com"
+user="ec2-user"
+
+echo "*****************************************\n"
+echo " Connecting to remote instance at $instance_address\n"
+echo "*****************************************\n"
+
+ssh -i "secrets/quote_reminder.pem" "$user@$instance_address"
