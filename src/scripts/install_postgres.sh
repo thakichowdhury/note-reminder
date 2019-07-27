88559d99bb9db415057de1719990a29b4697b2b4
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-27 16:12:15 -0700

    Add script to install postgres on aws ec2

---
+++ src/scripts/install_postgres.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "*****************************************"
+echo " Installing PostgreSQL"
+echo "*****************************************"
+
+sudo yum -y install postgresql postgresql-server postgresql-devel postgresql-contrib postgresql-docs
+sudo postgresql-setup initdb
+# Use MD5 Authentication
+sudo sed -i.bak -e 's/ident$/md5/' -e 's/peer$/md5/' /var/lib/pgsql9/data/pg_hba.conf
+#start
+sudo /sbin/chkconfig --levels 235 postgresql on
+sudo service postgresql start
+
+# http://imperialwicket.com/aws-install-postgresql-on-amazon-linux-quick-and-dirty
