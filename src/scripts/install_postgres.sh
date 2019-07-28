4ce001315515981b28b858692b4f4ad3aa8fc707
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-27 17:05:33 -0700

    Add credits at bottom of file

--- src/scripts/install_postgres.sh
+++ src/scripts/install_postgres.sh
@@ -12,4 +12,6 @@ sudo sed -i.bak -e 's/ident$/md5/' -e 's/peer$/md5/' /var/lib/pgsql9/data/pg_hba
 sudo /sbin/chkconfig --levels 235 postgresql on
 sudo service postgresql start
 
+# credit
+# https://gist.github.com/dstroot/2920991
 # http://imperialwicket.com/aws-install-postgresql-on-amazon-linux-quick-and-dirty
