a2b207e3388e3601422ee3e1e8d5a1405a371af7
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-28 23:15:04 -0700

    Update command to intialize postgres db

--- src/scripts/install_postgres.sh
+++ src/scripts/install_postgres.sh
@@ -8,7 +8,7 @@ sudo yum -y update
 
 # Intall PostgreSQL
 sudo yum -y install postgresql postgresql-server postgresql-devel postgresql-contrib postgresql-docs
-sudo postgresql-setup initdb
+sudo service postgresql initdb
 
 # Use MD5 Authentication
 sudo sed -i.bak -e 's/ident$/md5/' -e 's/peer$/md5/' /var/lib/pgsql9/data/pg_hba.conf
