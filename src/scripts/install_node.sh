03cc6399aa7392af625dabc7b83d4f04c366971b
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-28 22:37:08 -0700

    Add script to install node on remote linux box

---
+++ src/scripts/install_node.sh
@@ -0,0 +1,11 @@
+echo "Getting root priveleges"
+sudo -i
+
+# node version: https://github.com/nodesource/distributions/tree/master/rpm
+node_version=12
+
+echo "Getting node version $node_version"
+curl --silent --location "https://rpm.nodesource.com/setup_$node_version.x" | sudo bash -
+
+echo "Installing node version $node_version"
+sudo yum -y install nodejs
