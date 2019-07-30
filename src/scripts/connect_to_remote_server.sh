8a64806e3c889949464638fc9693b68918653c35
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-29 20:26:13 -0700

    Update remote server address

--- src/scripts/connect_to_remote_server.sh
+++ src/scripts/connect_to_remote_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-instance_address="ec2-34-207-167-220.compute-1.amazonaws.com"
+instance_address="ec2-3-89-10-242.compute-1.amazonaws.com"
 user="ec2-user"
 
 echo "*****************************************\n"
