ea9929684204f7e6e56d536bca5b2ee5cfba8dcf
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-30 00:10:04 -0700

    Add comment to explain cron break syntax

--- src/server/cron/sendMessageCron.js
+++ src/server/cron/sendMessageCron.js
@@ -2,7 +2,19 @@ const cron = require('node-cron');
 
 const sendMessage = require('../../db/actions/sendMessage');
 
-// run cron-job every 10 seconds
+/*
+
+ # ┌────────────── second (optional)
+ # │ ┌──────────── minute
+ # │ │ ┌────────── hour
+ # │ │ │ ┌──────── day of month
+ # │ │ │ │ ┌────── month
+ # │ │ │ │ │ ┌──── day of week
+ # │ │ │ │ │ │
+ # │ │ │ │ │ │
+ # * * * * * *
+
+*/
 const sendMessageCron = cron.schedule('0 0 */3 * * *', sendMessage);
 
 sendMessageCron.start();
