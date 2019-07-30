438f67f7e6b72048c3a7beb8d3733f113eec52dd
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-30 00:06:19 -0700

    Change cron job to send messages every 3 hours

--- src/server/cron/sendMessageCron.js
+++ src/server/cron/sendMessageCron.js
@@ -3,6 +3,6 @@ const cron = require('node-cron');
 const sendMessage = require('../../db/actions/sendMessage');
 
 // run cron-job every 10 seconds
-const sendMessageCron = cron.schedule('*/10 * * * * *', sendMessage);
+const sendMessageCron = cron.schedule('0 0 */3 * * *', sendMessage);
 
 sendMessageCron.start();
