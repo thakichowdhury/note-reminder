fd0f9601fa45e64a5d62e5767edc324384bc8262
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-31 08:47:26 -0700

    Update message sending function to send to the number attached to the note

--- src/db/actions/sendMessage.js
+++ src/db/actions/sendMessage.js
@@ -9,12 +9,12 @@ const sendMessage = async () => {
   const randomId = await fetchRandomNoteId();
 
   if (randomId) {
-    const query = 'SELECT message FROM notes WHERE id = $1;';
+    const query = 'SELECT phone_number, message FROM notes WHERE id = $1;';
     const values = [randomId];
 
-    const { rows: [{ message }] } = await db.query(query, values);
+    const { rows: [{ phone_number: phoneNumber, message }] } = await db.query(query, values);
 
-    createMessage({ message });
+    createMessage({ to: phoneNumber, message });
 
     updateLastCalled(randomId);
   }
