47076db7332c109b2ae3a7ce7d08685ecc50438b
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-31 08:44:38 -0700

    Update query to accomodate for if notes.last_called is NULL or if notes.created_at is in the same day

--- src/db/actions/fetchRandomNoteId.js
+++ src/db/actions/fetchRandomNoteId.js
@@ -3,7 +3,15 @@ const db = require('../../db');
 const getRandomInt = require('../../util/getRandomInt');
 
 const fetchRandomNoteId = async () => {
-  const { rows } = await db.query(`SELECT * FROM notes WHERE date_part('day', last_called) < date_part('day', NOW());`);
+  const { rows } = await db.query(
+    `
+      SELECT * FROM notes
+        WHERE 
+          date_part('day', last_called) < date_part('day', NOW())
+          OR date_part('day', created_at) < date_part('day', NOW())
+          OR last_called IS NULL;
+    `,
+  );
 
   const ids = rows.map(row => row.id);
   const randomInt = getRandomInt(0, ids.length - 1);
