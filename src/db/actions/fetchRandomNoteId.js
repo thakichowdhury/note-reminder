bdbd04587cdbde4b95bcfbd9d4a71d2e013b89c2
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-08-01 09:47:33 -0700

    Add TODO comment and return only id field values from fetching random note id

--- src/db/actions/fetchRandomNoteId.js
+++ src/db/actions/fetchRandomNoteId.js
@@ -4,8 +4,9 @@ const getRandomInt = require('../../util/getRandomInt');
 
 const fetchRandomNoteId = async () => {
   const { rows } = await db.query(
+    // TODO: add check to see if the user that saved the message has is_active to true
     `
-      SELECT * FROM notes
+      SELECT id FROM notes
         WHERE 
           date_part('day', last_called) < date_part('day', NOW())
           OR date_part('day', created_at) < date_part('day', NOW())
