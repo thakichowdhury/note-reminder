f8d438a585e1036b2d3103b6ca225cb611638527
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-31 21:02:11 -0700

    Create function to toggle a user's activity level

---
+++ src/db/actions/toggleUserActivity.js
@@ -0,0 +1,19 @@
+const db = require('../index');
+
+const getCurrentTimestamp = require('../../util/getCurrentTimestamp');
+
+const toggleUserActivity = async ({ phoneNumber }) => {
+  const modifiedAt = getCurrentTimestamp();
+
+  const query = 'UPDATE users SET is_active = NOT is_active, modified_at = $2 WHERE phone_number = $1 RETURNING phoneNumber';
+  const values = [phoneNumber, modifiedAt];
+
+  try {
+    const { rows } = await db.query(query, values);
+    return rows[0];
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = toggleUserActivity;
