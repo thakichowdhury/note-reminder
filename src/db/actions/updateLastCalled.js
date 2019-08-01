37ed224f3a71dc913d87905cbf47e35bcb605bcc
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-07-31 22:55:47 -0700

    Add semicolon to end of query

--- src/db/actions/updateLastCalled.js
+++ src/db/actions/updateLastCalled.js
@@ -5,7 +5,7 @@ const getCurrentTimestamp = require('../../util/getCurrentTimestamp');
 const updateLastCalled = async (notesId) => {
   const lastCalled = getCurrentTimestamp();
 
-  const query = 'UPDATE notes SET last_called = $1 WHERE id = $2 RETURNING id';
+  const query = 'UPDATE notes SET last_called = $1 WHERE id = $2 RETURNING id;';
   const values = [lastCalled, notesId];
 
   const { rows: [{ id }] } = await db.query(query, values);
