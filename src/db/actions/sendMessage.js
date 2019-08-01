8890579942c7fc3d235f34b83e87519b70e0690b
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-08-01 09:16:28 -0700

    Fix import path for twilioSendMessage

--- src/db/actions/sendMessage.js
+++ src/db/actions/sendMessage.js
@@ -3,7 +3,7 @@ const db = require('../../db');
 const fetchRandomNoteId = require('./fetchRandomNoteId');
 const updateLastCalled = require('./updateLastCalled');
 
-const createMessage = require('../../lib/twilio/createMessage');
+const createMessage = require('../../lib/twilio/twilioSendMessage');
 
 const sendMessage = async () => {
   const randomId = await fetchRandomNoteId();
